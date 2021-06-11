5921cd03cfa5893e273bb5eafe6b7a1626f9c5f6
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-06-11 13:14:20 -0500

    "1st commit: Steps to share work on gitHub from Eclipse."

---
+++ repoTest1/src/repoTest1/Main.java
@@ -0,0 +1,25 @@
+package repoTest1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("We created an immediate connection to gitHub on Eclipse. The  steps were: ");
+		System.out.println("1- In Eclipse, type git repo opn the top right of me;");
+		System.out.println("2- Go on gitHub online and create a repo;");
+		System.out.println("3- Copy the new repo;");
+		System.out.println("4- Click clone;");
+		System.out.println("5- The copied link will populate the box;");
+		System.out.println("6- Enter username and password, click next;");
+		System.out.println("7- You'll see the branches of your repo, click next again, then finish, you'll see it;");
+		System.out.println("8- Create the project");
+		System.out.println("9- Right click on project, and look for team, share project, enter the repo, it will populate, click finish;");
+		System.out.println("10- Start working on it;");
+		System.out.println("11- Cliock on team again and you'll see, everything: status, commit, push, etc;");
+		System.out.println();
+		
+
+	}
+
+}
